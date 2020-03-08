4ac06e18251bf3ea1c7472d46d8b8ae77f29ace1
julius <leonardojulius.jcl@gmail.com>
2020-03-08 22:28:55 +0800

    Picture included

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import './App.css';
 import {Layout, Header, Navigation, Drawer,Content} from 'react-mdl';
 import Main from './components/main';
 import { Link } from 'react-router-dom';
+import { Grid, Cell} from 'react-mdl';
+
 function App() {
   return (
     <div className="demo-big-content">
       <Layout>
-          <Header title="Title" scroll>
+          <Header className="header-color" title="Title"   scroll >
               <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
                 <Link to="/projects">Projects</Link>
                 <Link to="/contact">Contact</Link>
-              
+
 
               </Navigation>
           </Header>
@@ -27,8 +29,20 @@ function App() {
               </Navigation>
           </Drawer>
           <Content>
-              <div className="page-content" />
+          <div style = {{width: '100%', margin: 'auto'}}>
+          <Grid className="landing-grid">
+          <Cell col={12}>
+           <img
+             src=""
+           />
+          </Cell>
+          </Grid>
+          </div>
+
+             <div className="page-content" />
+
               <Main/>
+
           </Content>
       </Layout>
   </div>
