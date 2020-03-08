7e5a34257f79e04ce8eed0f6cf32eec467acbb0b
julius <leonardojulius.jcl@gmail.com>
2020-03-08 19:00:33 +0800

    Change name

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <h1>Julius Caesar Leonardo</h1>
+        <h1>Julius Leonardo</h1>
 
         </p>
         <a
