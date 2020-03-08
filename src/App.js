2660cc7a422e6ecd8baa77773b150213ebbaa840
julius <leonardojulius.jcl@gmail.com>
2020-03-09 00:05:14 +0800

    Final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import {Layout, Header, Navigation, Drawer,Content} from 'react-mdl';
 import Main from './components/main';
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="demo-big-content">
       <Layout>
-          <Header className="header-color" title="Title"   scroll >
+          <Header className="header-color" title="My Portfolio"   scroll >
               <Navigation>
                 <Link to="/resume">Resume</Link>
                 <Link to="/aboutme">About Me</Link>
@@ -22,41 +22,51 @@ function App() {
           </Header>
           <Drawer title="Title">
               <Navigation>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
-                  <a href="/">Link</a>
+
+                <Link to="/resume">Resume</Link>
+                <Link to="/aboutme">About Me</Link>
+                <Link to="/projects">Projects</Link>
+                <Link to="/contact">Contact</Link>
               </Navigation>
           </Drawer>
           <Content>
           <div style = {{width: '100%', margin: 'auto'}}>
           <Grid className="landing-grid">
           <Cell col={12}>
+          <img src={logo} className="App-logo" alt="logo" />
            <img
              src="https://lh3.googleusercontent.com/k_wVcUeqh8wEWAc6BYSHfTAwj8W8jiXxCqk0iiruMqcp3qfgG4EKX5Ha0xSTO96bmzWKPyNPVyzxqYq3te-vaPfqUGolPv5uuwry-d_44bE_VhNuv3u64Fdje9IX2WHUBXNxxjskNn1YlXSoi8xRLqaZ4XdtsECsMwArFTYqBeK0Xdh8cEhuD4w8Pdl8aTs8f7QUvbXAHkDwrIcOmaIJBOvdO-jPE-6qirOfWzdx37KnzIDFPZvEafBXT5AkNQgi2TNKyPH6AcNYeBjzDFLF-KA4MBLFTZD0oW3kxh5NaevV20yCHxfDSLTdTxJw1DUbG85FcqlTPiGqGGiVqH6ehSXEqOQ-2xsIi3Wh5BCuGPS6jKMiAKR4nK8y44qJOjYJf2hWy_NzIFJBgAz_PvbfNT5vu-evHMD8OFYvEiM_tScy2XsSef_nWwlmLAB69cwFd9V80zM_Du_VvEZg7HJ8xQMJ6t1_1vhxmHJN0dwv6K9QuMriH501MBM6kB2Zik92PnU6JP0DizRywt9beMQS3BEfhT55sICg8pjdHsMENeMJWXGew5dM8UrgvPLF1scGFCUW_HV9QpTTOaBqDkkVQkdqC1pXUCiBGSw3l6D5K2LPNkplcSOEFJzfSnvisy3DZ5vaSoo-QxJBmvZNthF5BZghabeafLYI3dSisLGudshM2LNgbJAOrIM=s337-no"
              alt="avatar"
              className="avatar-img"
            />
            <div className="banner-text">
-           <h1> Junior Web Developer</h1>
+           <h1>Julius Leonardo</h1>
+            <h4>Junior Web Developer</h4>
            <hr/>
+           <p>Powered by ReactJS</p>
 
-<p> | HTML/CSS |Materialize | Bootstrap | JavaScript | React | PHP-MVC | Yii2 | Laravel|</p>
+<p> | HTML/CSS | Materialize | Bootstrap | JavaScript | React | PHP-MVC | Yii2 | Laravel | MySql | Linux |</p>
+
 
 <div className="social-links">
 {/* linkedin*/}
-<a href="http://google.com" rel="noopener noreferrer" target ="_blank"></a>
-<i className="fa fa-linkedin-square" aria-hidden="true" />
+
+
+
 </div>
+
            </div>
 
+
+    <Main/>
           </Cell>
+
           </Grid>
           </div>
 
              <div className="page-content" />
 
-              <Main/>
+
 
 
 
