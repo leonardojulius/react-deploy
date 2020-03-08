81f131d4dca62e5f3dfc69d76d869dc7acf713a8
julius <leonardojulius.jcl@gmail.com>
2020-03-08 23:35:30 +0800

    profile pic

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
           <Grid className="landing-grid">
           <Cell col={12}>
            <img
-             src="https://raw.githubusercontent.com/leonardojulius/react-deploy/gh-pages/Screenshot%20from%202020-03-08%2022-25-38.png"
+             src="https://lh3.googleusercontent.com/k_wVcUeqh8wEWAc6BYSHfTAwj8W8jiXxCqk0iiruMqcp3qfgG4EKX5Ha0xSTO96bmzWKPyNPVyzxqYq3te-vaPfqUGolPv5uuwry-d_44bE_VhNuv3u64Fdje9IX2WHUBXNxxjskNn1YlXSoi8xRLqaZ4XdtsECsMwArFTYqBeK0Xdh8cEhuD4w8Pdl8aTs8f7QUvbXAHkDwrIcOmaIJBOvdO-jPE-6qirOfWzdx37KnzIDFPZvEafBXT5AkNQgi2TNKyPH6AcNYeBjzDFLF-KA4MBLFTZD0oW3kxh5NaevV20yCHxfDSLTdTxJw1DUbG85FcqlTPiGqGGiVqH6ehSXEqOQ-2xsIi3Wh5BCuGPS6jKMiAKR4nK8y44qJOjYJf2hWy_NzIFJBgAz_PvbfNT5vu-evHMD8OFYvEiM_tScy2XsSef_nWwlmLAB69cwFd9V80zM_Du_VvEZg7HJ8xQMJ6t1_1vhxmHJN0dwv6K9QuMriH501MBM6kB2Zik92PnU6JP0DizRywt9beMQS3BEfhT55sICg8pjdHsMENeMJWXGew5dM8UrgvPLF1scGFCUW_HV9QpTTOaBqDkkVQkdqC1pXUCiBGSw3l6D5K2LPNkplcSOEFJzfSnvisy3DZ5vaSoo-QxJBmvZNthF5BZghabeafLYI3dSisLGudshM2LNgbJAOrIM=s337-no"
              alt="avatar"
              className="avatar-img"
            />
